ecdd217241d7a4f9fa6c38fe2d2839b08a60d909
chang0731 <71751403+chang0731@users.noreply.github.com>
2020-11-18 16:03:07 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
-for (let i = 0; i <= 2; i++) {
-    led.plot(2 + i, 2)
-    led.plot(2 - i, 2)
-    led.plot(2, 2 + i)
-    led.plot(2, 2 - i)
-    basic.pause(100)
-}
+basic.forever(function () {
+    for (let x = 0; x <= 2; x++) {
+        for (let y = 0; y <= 2; y++) {
+            if (2 - x == 2 || 2 + (y - x) == 2) {
+                led.plot(2 + x, 2 - (y - x))
+                led.plot(2 + x, 2 + (y - x))
+            }
+        }
+        basic.pause(200)
+    }
+})
