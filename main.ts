1736d664d3314ed90c5ead8716505bb48867eae1
chang0731 <71751403+chang0731@users.noreply.github.com>
2020-11-18 16:04:40 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ basic.forever(function () {
         for (let y = 0; y <= 2; y++) {
             if (2 - x == 2 || 2 + (y - x) == 2) {
                 led.plot(2 + x, 2 - (y - x))
-                led.plot(2 + x, 2 + (y - x))
+                led.plot(2 - x, 2 + (y - x))
             }
         }
         basic.pause(200)
